fea5ac4eb0587cb71d349c4c566c45c9d689df30
YoussefNesafe <ynessafe@gmail.com>
2024-06-11 23:35:58 +0400

    add: pages structure

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutMePage = () => {
+  return (
+    <div>AboutMe Page</div>
+  )
+}
+
+export default AboutMePage
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,15 @@
+import PageTitle from '@/components/PageTitle'
+import React from 'react'
+
+const ProjectsPage = () => {
+  return (
+    <>
+      <PageTitle
+        title='Projects'
+        description='List of my projects'
+      />
+    </>
+  )
+}
+
+export default ProjectsPage
